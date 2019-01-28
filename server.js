000f0971db2b48a1eb206feeabdf47cad25f3b3b
danielbonifacio <danielbonifacio@outlook.com>
2019-01-28 10:43:15 -0200

    dev server

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ require('dotenv').config()
 const app = require('./src/app')
 
 console.clear()
-app.listen(4000, (err) => err ? console.log('erro') : console.log('rodando'))
+app.listen(4000, (err) =>
+    err
+        ? console.log('erro')
+        : console.log('API iniciada')
+)
