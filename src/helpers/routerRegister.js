5504c095d0d6cf362f7b64449a2493b8716aea21
danielbonifacio <danielbonifacio@outlook.com>
2019-01-28 09:32:05 -0200

    abstract route register

---
+++ src/helpers/routerRegister.js
@@ -0,0 +1,67 @@
+const http = require('axios')
+const { request } = require('./log')
+
+/**
+ * Recupera a URL do serviço
+ * @param {object} service Serviço
+ */
+const getUrlFromService = ({ host, port, https }) => `${https ? 'https' : 'http'}://${host}${port ? `:${port}` : ''}`
+
+/**
+ * Registra todas as rotas.
+ * ========================
+ * 
+ * @param {object} services serviços a serem registrados
+ * @param {object} router objeto router do express
+ */
+const register = (services, router) => {
+  services.map(service => {
+    const serviceUrl = getUrlFromService(service)
+    
+    router.all(`/${service.name}`, async function (req, res) {
+      
+      const url = serviceUrl + req.originalUrl
+      const method = req.method.toLowerCase()
+      const headers = req.headers
+      const data = req.body
+
+      // status que será feito o log
+      let status = 200
+
+      // requisita o serviço indicado
+      await http({
+          url,
+          method,
+          headers,
+          data
+        })
+        .then(response => {
+          res.status(response.status).send(response.data)
+        })
+        .catch(error => {
+          // caso haja uma resposta do serviço
+          if (error.response) {
+            const { response } = error
+            status = response.status
+            res
+              .status(status)
+              .send(response.data)
+          } else {
+            status = 500
+            res
+              .status(status)
+              .send({ message: `Não foi possível se conectar ao serviço ${service.name}` })
+          }
+        })
+
+      /**
+       * Loga as informações da requisição no servidor
+       * TODO: Deixar essa opção parametrizável
+       */
+      const log = request(req, service, status)
+      console.log(log)
+    })
+  })
+}
+
+module.exports = register
--- src/routes.js
+++ src/routes.js
@@ -1,57 +1,8 @@
 const { Router } = require('express')
-const axios = require('axios')
-const { request } = require('./helpers/log')
 const services = require('./services.map')
-
+const routerRegister = require('./helpers/routerRegister')
 const router = Router()
 
-const getUrlFromService = service => {
-  const { host, port, https } = service
-
-  return `${https ? 'https' : 'http'}://${host}${port ? `:${port}` : ''}`
-}
-
-
-services.map(service => {
-  const serviceUrl = getUrlFromService(service)
-
-  router
-    .all(`/${service.name}*`, async function (req, res) {
-  
-      const url = serviceUrl + req.originalUrl
-      const method = req.method.toLowerCase()
-      const headers = req.headers
-      const data = req.body
-
-      let status = 200
-  
-      await axios({
-        url,
-        method,
-        headers,
-        data
-      })
-        .then(({ data }) => {
-          res.status(200).send(data)
-        })
-        .catch(err => {
-          if (err.response) {
-            const { response } = err
-            res.status(response.status).send(response.data)
-            status = response.status
-          } else {
-            res.status(500).send({ message: 'Houve um erro ao se comunicar com a API' })
-            status = 500
-          }
-        })
-
-      /**
-       * Log da requisição no console
-       * Informa se houve sucesso ou erro
-       */
-      const log = request(req, service, status)
-      console.log(log)
-    })
-})
+routerRegister(services, router)
 
 module.exports = router
