79bbbfc563aa033fd13b13cc6ce45e94629c51fe
danielbonifacio <danielbonifacio@outlook.com>
2019-01-28 12:35:33 -0200

    simplify console logs

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -2,7 +2,7 @@ const moment = require('moment')
 
 const request = ({ method, originalUrl }, service, status) => {
     const date = moment().format('YYYY-MM-D h:mm:ss')
-    return `Date: ${date}\nMethod: ${method}\nService: ${service.name}\nEndpoint: ${originalUrl}\nStatus: ${status}\n---`
+    return `${date} ${method} ${originalUrl} ${status}`
 }
 
 module.exports = {
