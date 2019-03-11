e14a68ceb68c21e1a0030ad1ff5bf67af2271c9b
danielbonifacio <danielbonifacio@outlook.com>
2019-03-11 19:00:24 -0300

    default port to node environment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app = require('./src/app')
 
 console.clear()
-app.listen(4000, (err) =>
+app.listen(proccess.env.PORT || 4000, (err) =>
     err
         ? console.log('erro')
         : console.log('API iniciada')
